715dbfaa261a80812ec234116564fd5ba4002155
rev3nt <mikhail.merkulov.2005@mail.ru>
2025-09-26 19:22:05 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+def book_list_view(library_dict):
+    if len(library_dict) != 0:
+        for book in library_dict:
+            print(book)
+        return None
+
+    print('Библиотека пуста')
+
+    return None
+
+
+library = {
+    'Гарри Поттер и узник Азкабана': {'author': 'Джоан Роулинг', 'publication_year': 2021, 'available': True},
+    'Зеленая миля': {'author': 'Стивен Кинг', 'publication_year': 2025, 'available': False}
+}
+
+book_list_view(library)
