bc57f7964df4d7e2bef5aa141683398d04b660a0
rev3nt <mikhail.merkulov.2005@mail.ru>
2025-09-28 22:49:34 +0300

    add function added

--- main.py
+++ main.py
@@ -1,17 +1,38 @@
 def book_list_view(library_dict):
     if len(library_dict) != 0:
         for book in library_dict:
-            print(book)
-        return None
+            print(book, library_dict[book])
+    else:
+        print('Библиотека пуста')
 
-    print('Библиотека пуста')
 
-    return None
+def add_book(title, author, year):
+    if title in library:
+        print("Данная книга уже добавлена в библиотеку, хотите изменить информацию о ней(Y/N)? ")
+
+        user_answer = input().lower()
+
+        if user_answer == 'y':
+            library[title] = {'author': author, 'year': year, 'availability': None}
+
+            print(f'Редактирование книги \"{title}\" прошло успешно!')
+    else:
+        library[title] = {'author': author, 'year': year, 'availability': None}
+
+        print(f'Книга \"{title}\" успешно добавлена в библиотеку')
 
 
 library = {
     'Гарри Поттер и узник Азкабана': {'author': 'Джоан Роулинг', 'publication_year': 2021, 'available': True},
     'Зеленая миля': {'author': 'Стивен Кинг', 'publication_year': 2025, 'available': False}
 }
+
+book_list_view(library)
+
+add_book('Ванпис', 'Эйитиро Ода', 1980)
+
+book_list_view(library)
+
+add_book('Ванпис', 'Хидео Кадзима', 1984)
 
 book_list_view(library)
